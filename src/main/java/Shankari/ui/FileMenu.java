155a93efb9eb0b835962020fdc2e0de21ea27500
Jois Jyotisha <joisjyotisha@gmail.com>
2020-06-17 19:00:32 +0530

    Initial Commit

---
+++ src/main/java/Shankari/ui/FileMenu.java
@@ -0,0 +1,39 @@
+package Shankari.ui;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+
+public class FileMenu {
+    public MenuBar getMenuBar() {
+        // Create MenuBar
+        MenuBar menuBar = new MenuBar();
+
+        // Create menus
+        Menu fileMenu = new Menu("File");
+        Menu editMenu = new Menu("Edit");
+        Menu helpMenu = new Menu("Help");
+
+        // Create MenuItems
+        MenuItem newItem = new MenuItem("New");
+        MenuItem openFileItem = new MenuItem("Open File");
+        MenuItem exitItem = new MenuItem("Exit");
+
+        MenuItem copyItem = new MenuItem("Copy");
+        MenuItem pasteItem = new MenuItem("Paste");
+
+        //Actions on Menus
+        //newItem.setOnAction(event->NewInfo.takeInfo());
+        exitItem.setOnAction(event->System.exit(0));
+
+
+
+        // Add menuItems to the Menus
+        fileMenu.getItems().addAll(newItem, openFileItem, exitItem);
+        editMenu.getItems().addAll(copyItem, pasteItem);
+
+        // Add Menus to the MenuBar
+        menuBar.getMenus().addAll(fileMenu, editMenu, helpMenu);
+        menuBar.useSystemMenuBarProperty().set(true);
+        return menuBar;
+    }
+}
