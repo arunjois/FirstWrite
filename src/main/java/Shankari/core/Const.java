aa0df07c3e1d6853942f08a294c51952276c4436
Jois Jyotisha <joisjyotisha@gmail.com>
2020-06-20 22:23:36 +0530

    Written Const

---
+++ src/main/java/Shankari/core/Const.java
@@ -0,0 +1,9 @@
+package Shankari.core;
+
+public class Const {
+    public static final String SYS_DIR=System.getProperty("user.home");
+    public static final String HOME_DIR=SYS_DIR+"/.Shankari/";
+    public static final String EPHE_DIR=HOME_DIR+"ephe/";
+    public static final String GEONAMES = HOME_DIR+"geonames1000.sql";
+
+}
--- src/main/java/Shankari/core/Preliminary.java
+++ src/main/java/Shankari/core/Preliminary.java
@@ -2,6 +2,14 @@ package Shankari.core;
 import java.io.File;
 public class Preliminary {
     public void checkDir() {
-        
+        File home = new File(Const.HOME_DIR);
+        File ephe = new File(Const.EPHE_DIR);
+        File geoname = new File(Const.GEONAMES);
+       if(home.exists())
+           home.mkdir();
+       else if(ephe.exists())
+           ephe.mkdirs();
+       else if(geoname.exists())
+           ;//Write the file to the dir.
     }
 }
