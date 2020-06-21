dd6989f72309b8279c7910355d2010e3ea79b575
Jois Jyotisha <joisjyotisha@gmail.com>
2020-06-21 21:18:42 +0530

    Different Path for Linux and Windows along with Better file copy

--- src/main/java/Shankari/core/Const.java
+++ src/main/java/Shankari/core/Const.java
@@ -1,9 +1,24 @@
 package Shankari.core;
 
 public class Const {
-    public static final String SYS_DIR=System.getProperty("user.home");
-    public static final String HOME_DIR=SYS_DIR+"/.Shankari/";
-    public static final String EPHE_DIR=HOME_DIR+"ephe/";
-    public static final String GEONAMES = HOME_DIR+"geonames1000.sql";
+    public static String SYS_DIR;
+    public static String HOME_DIR;
+    public static String EPHE_DIR;
+    public static String GEONAMES;
+    public static void setDir() {
+        switch (System.getProperty("os.name")) {
+            case "Windows 10", "Windows 7":
+                SYS_DIR=System.getProperty("user.home");
+                HOME_DIR=SYS_DIR+"\\.Shankari\\";
+                EPHE_DIR=HOME_DIR+"ephe\\";
+                GEONAMES = HOME_DIR+"geonames1000.sql";
+            case "Linux":
+                SYS_DIR=System.getProperty("user.home");
+                HOME_DIR=SYS_DIR+"//.Shankari//";
+                EPHE_DIR=HOME_DIR+"ephe/";
+                GEONAMES = HOME_DIR+"geonames1000.sql";
+        }
+
+    }
 
 }
--- src/main/java/Shankari/core/Preliminary.java
+++ src/main/java/Shankari/core/Preliminary.java
@@ -1,15 +1,33 @@
 package Shankari.core;
+import java.awt.geom.IllegalPathStateException;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
 public class Preliminary {
     public void checkDir() {
         File home = new File(Const.HOME_DIR);
         File ephe = new File(Const.EPHE_DIR);
         File geoname = new File(Const.GEONAMES);
-       if(home.exists())
+       if(!home.exists())
            home.mkdir();
-       else if(ephe.exists())
+       else if(!ephe.exists())
            ephe.mkdirs();
-       else if(geoname.exists())
-           ;//Write the file to the dir.
+       else if(!geoname.exists())
+           writeDatabaseFile();
+
+
+    }
+    public void writeDatabaseFile(){
+        try{
+            Files.copy(Paths.get("E:\\Arun\\geonames1000.sql"),
+                    Paths.get(Const.GEONAMES), StandardCopyOption.REPLACE_EXISTING);
+        }
+        catch (IOException e){
+            System.out.println("Error in Writing Database File"+e.getStackTrace());
+        }
+
     }
 }
