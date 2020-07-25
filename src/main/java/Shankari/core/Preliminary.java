52b853bc75a75a5758cd63e55c7fae29c2d7f56d
Arun Jois <arun.joiss@gmail.com>
2020-07-25 22:40:23 +0530

    [ADD] Swisseph

--- src/main/java/Shankari/core/Preliminary.java
+++ src/main/java/Shankari/core/Preliminary.java
@@ -5,10 +5,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.util.Iterator;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Preliminary {
     public Path dir = Paths.get(Const.DIR);
